e731d0a054152da223d6f76385c0b6da6f6b537c
Lucas Souto <lucas.souto@taglivros.com.br>
2019-07-18 11:26:30 -0300

    Add pre-commit hooks and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='pylava-pre-commit',
+    description='Pylava pre-commit',
+    url='https://github.com/lucassouto/pylava-pre-commit',
+    version='0.0.1',
+    author='Lucas Souto',
+    author_email='lucasinfologos@gmail.com',
+    install_requires=[
+        'pylama',
+    ],
+ )
+
